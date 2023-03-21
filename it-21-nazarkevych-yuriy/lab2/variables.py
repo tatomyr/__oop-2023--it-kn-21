9a6eed86a144a7efee3408549f31857268faad52
YaDaniels <dan4ik.zhovkva99@gmail.com>
2023-03-21 23:20:13 +0200

    labs+readme

---
+++ it-21-nazarkevych-yuriy/lab2/variables.py
@@ -0,0 +1,10 @@
+mystring = "dopobachennya"
+myfloat = 99.99
+myint = 99
+
+if mystring == "dopobachennya":
+    print("String: %s" % mystring)
+if isinstance(myfloat, float) and myfloat == 99.99:
+    print("Float: %f" % myfloat)
+if isinstance(myint, int) and myint == 99:
+    print("Integer: %d" % myint)
