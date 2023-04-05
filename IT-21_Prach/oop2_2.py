51bd273b9a7caed08916adda0464c8e4e583ddbf
nastya <prachnastya@gmail.com>
2023-04-05 22:25:39 +0300

    oop2.md

---
+++ IT-21_Prach/oop2_2.py
@@ -0,0 +1,3 @@
+my_numbers = [100, 150, 200, 250, 300]
+for num in my_numbers:
+    print(num)
---
+++ IT-21_Prach/oop2_3.py
@@ -0,0 +1,4 @@
+pi = 3.14159 
+radius = float(input('Введіть радіус кола: '))
+area = pi * radius ** 2
+print("Площа кола з радіусом", radius, 'дорівнює', area)
