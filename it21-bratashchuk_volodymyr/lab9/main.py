96bb610777419abc89ea6b30631326ae98f9f028
Vova <vovadorko@gmail.com>
2023-05-17 00:26:01 +0300

    bunch of labs

---
+++ it21-bratashchuk_volodymyr/lab9/main.py
@@ -0,0 +1,48 @@
+class Dog():
+    def __init__(self, name, age, breed, owner):
+        self.name = name
+        self.age = age
+        self.breed = breed
+        self.owner = owner
+
+    def bark(self):
+        print(self.name + ": Woof!")
+
+    def get_older(self):
+        self.age += 1
+
+    def get_age(self):
+        return self.age
+
+    def change_owner(self, new_owner):
+        self.owner = new_owner
+
+    def __repr__(self) -> str:
+        return f"Dog({self.name}, {self.age}, {self.breed}, {self.owner})"
+    
+    def __str__(self) -> str:
+        return f"{self.name} is {self.age} years old, {self.breed} breed, owner is {self.owner}"
+    
+    def __add__(self, other):
+        return self.age + other.age
+    
+    def __sub__(self, other):
+        return self.age - other.age
+    
+    def __len__(self):
+        return len(self.name)
+    
+
+dog = Dog("Rex", 3, "Labrador", "ID")
+dog1 = Dog("Jack", 5, "Pitbull", "Igor")
+
+# Displaying information about dog
+print(repr(dog))
+print(dog)
+
+# Addind and sustracting dogs' age
+print(dog + dog1)
+print(dog1 - dog)
+
+# Displaying lenth of dog's name
+print(len(dog))
