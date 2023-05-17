5c7ec1f8ed3567394f05059ab9fa531998d9b473
Potsilujka <123180481+potsilujka@users.noreply.github.com>
2023-05-17 11:41:02 +0300

    Update lab7.py

--- lab7/lab7.py
+++ lab7/lab7.py
@@ -1,24 +1,6 @@
 
 class Employee:
 
-    def __init__(self, first, last, pay):
-        self.first = first
-        self.last = last
-        self.email = first + '.' + last + '@email.com'
-        self.pay = pay
-
-    def fullname(self):
-        return '{} {}'.format(self.first, self.last)
-
-emp_1 = Employee('Corey', 'Schafer', 50000)
-emp_2 = Employee('Test', 'Employee', 60000)
-
-
-
-
-
-class Employee:
-
     num_of_emps = 0
     raise_amt = 1.04
 
