e3b7a012c5db1c87c23a537a0acdb0649b6ccf8c
Potsilujka <123180481+potsilujka@users.noreply.github.com>
2023-05-17 11:29:50 +0300

    Update lab8.py

--- lab8/lab8.py
+++ lab8/lab8.py
@@ -16,20 +16,44 @@ class Employee:
         self.pay = int(self.pay * self.raise_amt)
 
 
-dev_1 = Employee('Corey', 'Schafer', 50000)
-dev_2 = Employee('Test', 'Employee', 60000)
+class Developer(Employee):
+    raise_amt = 1.10
 
-print(dev_1.email)
-print(dev_2.email)
-
-
-
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
 
 
+class Manager(Employee):
+
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
 
 
+dev_1 = Developer('Corey', 'Schafer', 50000, 'Python')
+dev_2 = Developer('Test', 'Employee', 60000, 'Java')
 
+mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
 
-# print(dev_1.raise_amt)
-# dev_1.apply_raise()
-# print(dev_1.raise_amt)
+print(mgr_1.email)
+
+mgr_1.add_emp(dev_2)
+mgr_1.remove_emp(dev_2)
+
+mgr_1.print_emps()
