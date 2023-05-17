fc205998488b6160b6abf65472c274fcb3526b4e
Potsilujka <123180481+potsilujka@users.noreply.github.com>
2023-05-17 11:39:51 +0300

    Update lab10.py

--- lab10/lab10.py
+++ lab10/lab10.py
@@ -1,36 +1,36 @@
 
 class Employee:
 
-    raise_amt = 1.04
-
-    def __init__(self, first, last, pay):
+    def __init__(self, first, last):
         self.first = first
         self.last = last
-        self.email = first + '.' + last + '@email.com'
-        self.pay = pay
 
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
+
+    @property
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
-
-    def apply_raise(self):
-        self.pay = int(self.pay * self.raise_amt)
-
-    def __repr__(self):
-        return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
-
-    def __str__(self):
-        return '{} - {}'.format(self.fullname(), self.email)
-
-    def __add__(self, other):
-        return self.pay + other.pay
-
-    def __len__(self):
-        return len(self.fullname())
+    
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
+    
+    @fullname.deleter
+    def fullname(self):
+        print('Delete Name!')
+        self.first = None
+        self.last = None
 
 
-emp_1 = Employee('Corey', 'Schafer', 50000)
-emp_2 = Employee('Test', 'Employee', 60000)
+emp_1 = Employee('John', 'Smith')
+emp_1.fullname = "Corey Schafer"
 
-# print(emp_1 + emp_2)
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname)
 
-print(len(emp_1))
+del emp_1.fullname
