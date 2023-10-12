cdf89bed5e218e9bd4599665a6eaee0350c2f3fa
Kuchkuda Maksum <maksum172090@gmail.com>
2023-10-12 20:57:35 +0300

    labssss

--- it21_Kuchkuda_Maksum/lab6/class_Emp.py
+++ it21_Kuchkuda_Maksum/lab6/class_Emp.py
@@ -1,25 +1,17 @@
-class Employee:
-    num_of_emps = 0
+class Employee: 
 
     def __init__(self, first, last, pay):
-        self.first = first 
-        self.last = last 
-        self.pay = pay 
+        self.first = first
+        self.last = last
+        self.pay = pay
         self.email = first + '.' + last + '@company.com'
         
-        Employee.num_of_emps += 1
-
     def fullname(self):
-        return '{} {}'.format(self.first, self.last) 
-    
-    def namepay(self):
-        return '{} {}'.format(self.first, self.pay)
-    
-emp_1 = Employee('Corey', 'Schafer', 50000)
+        return '{} {}'.format(self.first, self.last)
+        
+
+emp_1 = Employee('Corney', 'Schafer', 50000)
 emp_2 = Employee('Test', 'User', 60000)
+
+emp_1.fullname()
 print(Employee.fullname(emp_1))
-print(Employee.namepay(emp_1))
-print(emp_1.email)
-print(emp_1.pay)
-#print('{} {}'.format(emp_1.first, emp_1.last))
-print(Employee.num_of_emps)
--- it21_Kuchkuda_Maksum/lab7/class_variables_classmethods_and_staticmethods.py
+++ it21_Kuchkuda_Maksum/lab7/class_variables_classmethods_and_staticmethods.py
@@ -1,60 +1,53 @@
 class Employee:
 
     num_of_emps = 0
-    raise_amt = 1.04
+    raise_emount = 1.04
 
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
-        self.email = first + '.' + last + '@email.com'
+        self.email = first + '.' + last + '@company.com'
         self.pay = pay
 
-        Employee.num_of_emps += 1
-
+        Employee.num_of_emps += 1 
+        
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
-
-    def apply_raise(self):
-        self.pay = int(self.pay * self.raise_amt)
+        
+    def appy_raise(self):
+        self.pay = int(self.pay * self.raise_emount)
 
     @classmethod
-    def set_raise_amt(cls, amount):
-        cls.raise_amt = amount
-
+    def set_raise_amt(cls, amout):
+        cls.raise_emount = amout
+    
     @classmethod
     def from_string(cls, emp_str):
         first, last, pay = emp_str.split('-')
         return cls(first, last, pay)
-
+    
     @staticmethod
     def is_workday(day):
         if day.weekday() == 5 or day.weekday() == 6:
             return False
         return True
 
-
-emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_1 = Employee('Corney', 'Schafer', 50000)
 emp_2 = Employee('Test', 'Employee', 60000)
 
-Employee.set_raise_amt(1.05)
-
-print(Employee.raise_amt)
-print(emp_1.raise_amt)
-print(emp_2.raise_amt)
-
-emp_str_1 = 'John-Doe-70000'
-emp_str_2 = 'Steve-Smith-30000'
-emp_str_3 = 'Jane-Doe-90000'
-
-first, last, pay = emp_str_1.split('-')
-
-#new_emp_1 = Employee(first, last, pay)
-new_emp_1 = Employee.from_string(emp_str_1)
-
-print(new_emp_1.email)
-print(new_emp_1.pay)
-
 import datetime
-my_date = datetime.date(2016, 7, 11)
+my_date = datetime.date(2016, 7, 10)
+
+emp_1.appy_raise()
+emp_2.appy_raise()
+
+emp_str = "John-Doe-70000"
+emp_3 = Employee.from_string(emp_str)
+print(emp_3.first)  # Output: John
+print(emp_3.last)   # Output: Doe
+print(emp_3.pay)    # Output: 70000
+
+print(emp_1.pay)  
+print(emp_2.pay)
 
 print(Employee.is_workday(my_date))
