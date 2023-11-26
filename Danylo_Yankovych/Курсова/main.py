2bb9eafe8ffe1f0d3369648b4d118541e37ebfa8
YaDaniels <125445280+YaDaniels@users.noreply.github.com>
2023-11-26 19:59:45 +0200

    -

--- Danylo_Yankovych/Курсова/main.py
+++
@@ -1,6 +0,0 @@
-from ui import UI
-
-# Початок виконання програми
-if __name__ == "__main__":
-    ui = UI()
-    ui.run()
--- Danylo_Yankovych/Курсова/retail_sale.py
+++
@@ -1,8 +0,0 @@
-class RetailSale:
-    def __init__(self, fertilizer, quantity_sold, sale_date):
-        self.fertilizer = fertilizer
-        self.quantity_sold = quantity_sold
-        self.sale_date = sale_date
-
-    def display_sale_info(self):
-        print(f"Дата продажу: {self.sale_date.strftime('%Y-%m-%d')}, {self.quantity_sold} kg продано {self.fertilizer.name}")
